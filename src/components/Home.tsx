acf60a66a73d3629aa818e2fd64a6db6bc8ba027
Jvmliya <jamilaa.guliyeva@gmail.com>
2025-07-08 19:08:04 +0400

    afasfas

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,24 @@ import "./Home.scss";
 
 const Home = () => {
     return (
-        <div>
-asfasf           
+        <div className="container">
+            <div className="col">
+
+                <div className="image">
+                    <img
+                        src="https://tap.azstatic.com/uploads/medium/2025%2F04%2F29%2F00%2F53%2F47%2F127d9d8c-ade7-4cf6-825a-fa83b971746f%2F27314_nl5hK4zSmPDP2ykAjfmWfg.jpg" />
+                </div>
+
+                <div className="text">
+                    <h4>36 000 AZN</h4>
+                    <p>20 sot torpaq sahesi, Qusar rayonu</p>
+                    <span>Baki, bu gun 15:00</span>
+                </div>
+
+            </div>
         </div>
+
+
     )
 }
 export default Home
