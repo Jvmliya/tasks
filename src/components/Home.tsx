86864f9f89632fa6db6e5b9bf70d5a2ab4eb5ad9
Jvmliya <jamilaa.guliyeva@gmail.com>
2025-07-09 00:01:04 +0400

    afsafasf

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,27 +1,58 @@
 import "./Home.scss";
+import { CiTwitter } from "react-icons/ci";
+
 
 const Home = () => {
     return (
         <div className="container">
-            {Array.from({ length: 16 }).map((_, index) => (
-                <div className="col" key={index}>
-                    <div className="image">
-                        <img
-                            src="https://tap.azstatic.com/uploads/medium/2025%2F04%2F29%2F00%2F53%2F47%2F127d9d8c-ade7-4cf6-825a-fa83b971746f%2F27314_nl5hK4zSmPDP2ykAjfmWfg.jpg"
-                            alt="product"
-                        />
-                    </div>
 
-                    <div className="text">
-                        <h4>36 000 AZN</h4>
-                        <p>20 sot torpaq sahəsi, Qusar rayonu</p>
-                        <span>Bakı, bu gün 15:00</span>
+            <div className="main">
+
+                <div className="leftContainer">
+                    <div className="leftImage">
+                        <img src="https://plus.unsplash.com/premium_photo-1688350808212-4e6908a03925?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt="girl" />
                     </div>
                 </div>
-            ))}
+
+                <div className="rightContainer">
+
+                    <div className="forUser">
+                        <h3>Sarah Anderson</h3>
+                        <button> Product Designer</button>
+                    </div>
+
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque architecto soluta rerum assumenda. Necessitatibus, a consectetur architecto dicta repellat fuga libero deserunt accusamus numquam error, nesciunt eligendi quisquam quae earum.</p>
+
+                    <div className='contact'>
+
+                        <div>
+                            <p>icon</p>
+                            <p>contact</p>
+                        </div>
+
+                        <div>
+                            <p>icon</p>
+                            <p>follow</p>
+                        </div>
+
+                    </div>
+
+                    <div className="btmright">
+                        <div className="icons">
+                            <CiTwitter />
+                            <CiTwitter />
+                            <CiTwitter />
+                        </div>
+
+                        <div className="location">
+                            <CiTwitter /> shsdajlfwkfjhajs
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
-
-
     )
 }
 export default Home
